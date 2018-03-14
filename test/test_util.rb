7cd0639f5234863824a7cf1fc1629d306343b0eb
qtakamitsu <qtakamitsu@gmail.com>
2018-03-14 22:54:25 +0900

    add Mrb::Util.git_clone_command test

--- test/test_util.rb
+++ test/test_util.rb
@@ -15,4 +15,14 @@ class MrbTest < Test::Unit::TestCase
     assert_equal(actual[:full_name], 'mruby-aaa')
     assert_equal(actual[:internal_name], 'mruby_aaa')
   end
+
+  def test_git_clone_command
+    actual = Mrb::Util.git_clone_command "https://example.com/",  "1.0.0", "path/to"
+    assert_equal actual, "git clone --depth=1  -b 1.0.0  https://example.com/ path/to/1.0.0" 
+  end
+
+  def test_git_clone_command_master
+    actual = Mrb::Util.git_clone_command "https://example.com/",  "master", "path/to"
+    assert_equal actual, "git clone --depth=1  https://example.com/ path/to/master" 
+  end
 end
